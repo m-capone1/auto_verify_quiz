a6f23e72a56fd59476fd8f7765fd866b7a749191
Maddy Capone <maddycapone@gmail.com>
2024-11-30 14:01:31 -0500

    updated vehicle controller with update vehicle functionality

--- controllers/vehiclesController.js
+++ controllers/vehiclesController.js
@@ -85,6 +85,75 @@ const getVehicles = async (req, res) => {
 
 const updateVehicle = async (req, res) => {
   try {
+    const { year, make, model, trim } = req.body;
+    const vehicleId = parseInt(req.params.id);
+
+    if (!year || !make || !model || !trim || !vehicleId) {
+      return res.status(400).json({
+        error: e,
+        message:
+          "Missing required fields. Please provide year, make, model, and trim in the request body.",
+      });
+    }
+
+    let vehicles = [];
+
+    try {
+      const vehiclesData = await fs.readFileSync(jsonPath, "utf8");
+      vehicles = JSON.parse(vehiclesData);
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({
+        error: "Unable to read file.",
+        message: err.message,
+      });
+    }
+
+    const updateVehicle = {
+      id: vehicleId,
+      year,
+      make,
+      model,
+      trim,
+    };
+
+    const findVehicle = vehicles.find((vehicle) => vehicle.id === vehicleId);
+
+    if (!findVehicle) {
+      return res.status(404).json({
+        error: "Vehicle not found.",
+        message: `Vehicle with ID ${vehicleId} not found.`,
+      });
+    }
+
+    let vehicleFilter = [];
+    vehicles.forEach((vehicle) => {
+      if (vehicle.id !== vehicleId) {
+        vehicleFilter.push(vehicle);
+      } else {
+        vehicleFilter.push(updateVehicle);
+      }
+    });
+
+    fs.writeFile(
+      jsonPath,
+      JSON.stringify(vehicleFilter, null, 2),
+      "utf8",
+      (err) => {
+        if (err) {
+          console.error("Error writing to file:", err);
+          return res.status(500).json({
+            error: "Unable to write to file.",
+            message: err.message,
+          });
+        }
+
+        return res.status(200).json({
+          message: `Successfully updated vehicle with ID ${vehicleId}.`,
+          vehicle: updateVehicle,
+        });
+      }
+    );
   } catch (e) {}
 };
 
